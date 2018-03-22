091763efe3cbbcddc11dcb5990ed1b4a541f5b61
IllidanS4 <illidans4@gmail.com>
2018-03-22 15:46:46 +0100

    thread_sleep, thread_id

--- plugins/src/natives/nthread.cpp
+++ plugins/src/natives/nthread.cpp
@@ -1,5 +1,8 @@
 #include "../natives.h"
 #include "../context.h"
+#include "../strings.h"
+#include <thread>
+#include <sstream>
 
 namespace Natives
 {
@@ -26,6 +29,33 @@ namespace Natives
 		amx_RaiseError(amx, AMX_ERR_SLEEP);
 		return -2;
 	}
+
+	// native thread_sleep(ms);
+	static cell AMX_NATIVE_CALL thread_sleep(AMX *amx, cell *params)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(params[1]));
+		return 0;
+	}
+
+	// native thread_id(id[], size=sizeof id);
+	static cell AMX_NATIVE_CALL thread_id(AMX *amx, cell *params)
+	{
+		std::ostringstream buf;
+		buf << std::this_thread::get_id();
+		cell *addr;
+		amx_GetAddr(amx, params[1], &addr);
+		std::string str = buf.str();
+		amx_SetString(addr, str.c_str(), false, false, params[2]);
+		return str.size();
+	}
+
+	// native String:thread_id_s();
+	static cell AMX_NATIVE_CALL thread_id_s(AMX *amx, cell *params)
+	{
+		std::ostringstream buf;
+		buf << std::this_thread::get_id();
+		return reinterpret_cast<cell>(strings::create(buf.str(), true));
+	}
 }
 
 static AMX_NATIVE_INFO native_list[] =
@@ -33,6 +63,9 @@ static AMX_NATIVE_INFO native_list[] =
 	AMX_DECLARE_NATIVE(thread_detach),
 	AMX_DECLARE_NATIVE(thread_attach),
 	AMX_DECLARE_NATIVE(thread_sync),
+	AMX_DECLARE_NATIVE(thread_sleep),
+	AMX_DECLARE_NATIVE(thread_id),
+	AMX_DECLARE_NATIVE(thread_id_s),
 };
 
 int RegisterThreadNatives(AMX *amx)
