e9bea7156cf31fd22fef463194c1c2fcfea3275b
IllidanS4 <illidans4@gmail.com>
2018-10-20 12:10:49 +0200

    linux fix

--- plugins/src/natives.h
+++ plugins/src/natives.h
@@ -17,31 +17,40 @@
       else (result) = NULL;                                                 \
     } while (0)
 
-template <AMX_NATIVE Native>
-struct native_info;
+namespace impl
+{
+	template <AMX_NATIVE Native>
+	struct native_info;
+
+	template <AMX_NATIVE Native>
+	static cell AMX_NATIVE_CALL adapt_native(AMX *amx, cell *params)
+	{
+		if(params[0] < native_info<Native>::arg_count * static_cast<cell>(sizeof(cell)))
+		{
+			logerror(amx, AMX_ERR_PARAMS, "[PP] %s: not enough arguments (%d expected, got %d)", native_info<Native>::name, native_info<Native>::arg_count, params[0] / static_cast<cell>(sizeof(cell)));
+			return 0;
+		}
+		return Native(amx, params);
+	}
+}
 
 #define AMX_DEFINE_NATIVE(Name, ArgCount) \
-	static cell AMX_NATIVE_CALL Name(AMX *amx, cell *params); \
+	cell AMX_NATIVE_CALL Name(AMX *amx, cell *params); \
+} \
+namespace impl \
+{ \
 	template <> \
-	struct ::native_info<&Name> \
+	struct native_info<&Natives::Name> \
 	{ \
 		static constexpr const char name[] = #Name; \
 		static constexpr const cell arg_count = ArgCount; \
 	}; \
-	static cell AMX_NATIVE_CALL Name(AMX *amx, cell *params)
+} \
+namespace Natives \
+{ \
+	cell AMX_NATIVE_CALL Name(AMX *amx, cell *params)
 
-template <AMX_NATIVE Native>
-static cell AMX_NATIVE_CALL adapt_native(AMX *amx, cell *params)
-{
-	if(params[0] < native_info<Native>::arg_count * static_cast<cell>(sizeof(cell)))
-	{
-		logerror(amx, AMX_ERR_PARAMS, "[PP] %s: not enough arguments (%d expected, got %d)", native_info<Native>::name, native_info<Native>::arg_count, params[0] / static_cast<cell>(sizeof(cell)));
-		return 0;
-	}
-	return Native(amx, params);
-}
-
-#define AMX_DECLARE_NATIVE(Name) {#Name, ::adapt_native<&Natives::Name>}
+#define AMX_DECLARE_NATIVE(Name) {#Name, impl::adapt_native<&Natives::Name>}
 
 int RegisterConfigNatives(AMX *amx);
 int RegisterPawnNatives(AMX *amx);
