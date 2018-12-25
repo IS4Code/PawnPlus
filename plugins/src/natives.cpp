0b0278e66da5cd17e1d0fa77a6927e2db3e41805
IllidanS4 <illidans4@gmail.com>
2018-12-25 13:27:03 +0100

    custom error handling

--- plugins/src/natives.cpp
+++ plugins/src/natives.cpp
@@ -2,6 +2,28 @@
 
 cell impl::handle_error(AMX *amx, cell *params, const char *native, const errors::native_error &error)
 {
+	int handler;
+	if(amx_FindPublic(amx, "pp_on_error", &handler) == AMX_ERR_NONE)
+	{
+		cell amx_addr, *addr;
+		cell reset_hea, *ret_addr;
+		amx_Allot(amx, 1, &reset_hea, &ret_addr);
+		*ret_addr = 0;
+		amx_Push(amx, error.code);
+		amx_Push(amx, reset_hea);
+		amx_PushString(amx, &amx_addr, &addr, error.message.c_str(), false, false);
+		amx_PushString(amx, &amx_addr, &addr, native, false, false);
+		cell ret;
+		amx_Exec(amx, &ret, handler);
+		cell retval = *ret_addr;
+		amx_Release(amx, reset_hea);
+
+		if(ret)
+		{
+			return retval;
+		}
+	}
+
 	logprintf("[PP] %s: %s", native, error.message.c_str());
 	if(error.code)
 	{
