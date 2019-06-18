a746634218120836ac2af39c389c278e03c6bb60
IllidanS4 <illidans4@gmail.com>
2019-06-18 11:52:52 +0200

    v1.0.1

--- plugins/src/main.h
+++ plugins/src/main.h
@@ -4,8 +4,8 @@
 #include "sdk/amx/amx.h"
 #include <utility>
 
-#define PP_VERSION_STRING "v1.0.0"
-#define PP_VERSION_NUMBER 0x100
+#define PP_VERSION_STRING "v1.0.1"
+#define PP_VERSION_NUMBER 0x101
 
 typedef void(*logprintf_t)(const char* format, ...);
 extern logprintf_t logprintf;
