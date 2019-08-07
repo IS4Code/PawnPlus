b3ecff772b6312c874e592c41a04b1f95b68543f
IllidanS4 <illidans4@gmail.com>
2019-08-07 20:34:46 +0200

    stored parameters can capture handles and expressions

--- plugins/src/objects/stored_param.cpp
+++ plugins/src/objects/stored_param.cpp
@@ -1,5 +1,8 @@
 #include "stored_param.h"
+#include "modules/variants.h"
 #include "errors.h"
+#include <cstring>
+#include <limits>
 
 stored_param stored_param::create(AMX *amx, char format, const cell *args, size_t &argi, size_t numargs)
 {
@@ -7,21 +10,53 @@ stored_param stored_param::create(AMX *amx, char format, const cell *args, size_
 	switch(format)
 	{
 		case 'a':
+		{
 			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 2);
 			addr = amx_GetAddrSafe(amx, args[argi]);
 			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
 			addr2 = amx_GetAddrSafe(amx, args[argi]);
 			return stored_param(std::basic_string<cell>(addr, *addr2));
+		}
 		case 's':
+		{
 			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
 			addr = amx_GetAddrSafe(amx, args[argi]);
 			return stored_param(std::basic_string<cell>(addr));
+		}
 		case 'e':
+		{
 			return stored_param();
+		}
+		case 'v':
+		{
+			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
+			addr = amx_GetAddrSafe(amx, args[argi]);
+			dyn_object *ptr;
+			if(!variants::pool.get_by_id(*addr, ptr)) amx_LogicError(errors::pointer_invalid, "variant", *addr);
+			return stored_param(*ptr);
+		}
+		case 'h':
+		{
+			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
+			addr = amx_GetAddrSafe(amx, args[argi]);
+			std::shared_ptr<handle_t> ptr;
+			if(!handle_pool.get_by_id(*addr, ptr)) amx_LogicError(errors::pointer_invalid, "handle", *addr);
+			return stored_param(std::move(ptr));
+		}
+		case 'x':
+		{
+			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
+			addr = amx_GetAddrSafe(amx, args[argi]);
+			expression_ptr ptr;
+			if(!expression_pool.get_by_id(*addr, ptr)) amx_LogicError(errors::pointer_invalid, "expression", *addr);
+			return stored_param(std::move(ptr));
+		}
 		default:
+		{
 			if(++argi >= numargs) throw errors::end_of_arguments_error(args, numargs + 1);
 			addr = amx_GetAddrSafe(amx, args[argi]);
 			return stored_param(*addr);
+		}
 	}
 }
 
@@ -41,6 +76,26 @@ void stored_param::push(AMX *amx, int id) const
 			std::memcpy(phys_addr, string_value.c_str(), (string_value.size() + 1) * sizeof(cell));
 			amx_Push(amx, addr);
 			break;
+		case param_type::variant_param:
+			amx_Push(amx, variant_value.store(amx));
+			break;
+		case param_type::handle_param:
+			amx_Push(amx, handle_value->get().store(amx));
+			break;
+		case param_type::expression_param:
+			try{
+				auto result = expr_value->execute(expression::args_type(), expression::exec_info(amx));
+				amx_Push(amx, result.store(amx));
+			}catch(const errors::native_error &err)
+			{
+				logprintf("[PawnPlus] Unhandled error in stored parameter: %s", err.message.c_str());
+				amx_Push(amx, std::numeric_limits<cell>::min());
+			}catch(const errors::amx_error &err)
+			{
+				logprintf("[PawnPlus] Unhandled AMX error %d in stored parameter: %s", err.code, amx::StrError(err.code));
+				amx_Push(amx, std::numeric_limits<cell>::min());
+			}
+			break;
 	}
 }
 
@@ -59,23 +114,62 @@ stored_param::stored_param(std::basic_string<cell> &&str) noexcept : type(param_
 
 }
 
+stored_param::stored_param(const dyn_object &obj) noexcept : type(param_type::variant_param), variant_value(obj)
+{
+
+}
+
+stored_param::stored_param(std::shared_ptr<handle_t> &&handle) noexcept : type(param_type::handle_param), handle_value(std::move(handle))
+{
+
+}
+
+stored_param::stored_param(expression_ptr &&expr) noexcept : type(param_type::expression_param), expr_value(std::move(expr))
+{
+
+}
+
 stored_param::stored_param(const stored_param &obj) noexcept : type(obj.type)
 {
-	if(type == param_type::string_param)
+	switch(type)
 	{
-		new (&string_value) std::basic_string<cell>(obj.string_value);
-	}else{
-		cell_value = obj.cell_value;
+		case param_type::cell_param:
+			cell_value = obj.cell_value;
+			break;
+		case param_type::string_param:
+			new (&string_value) std::basic_string<cell>(obj.string_value);
+			break;
+		case param_type::variant_param:
+			new (&variant_value) dyn_object(obj.variant_value);
+			break;
+		case param_type::handle_param:
+			new (&handle_value) std::shared_ptr<handle_t>(obj.handle_value);
+			break;
+		case param_type::expression_param:
+			new (&expr_value) expression_ptr(obj.expr_value);
+			break;
 	}
 }
 
 stored_param::stored_param(stored_param &&obj) noexcept : type(obj.type)
 {
-	if(type == param_type::string_param)
+	switch(type)
 	{
-		new (&string_value) std::basic_string<cell>(std::move(obj.string_value));
-	}else{
-		cell_value = obj.cell_value;
+		case param_type::cell_param:
+			cell_value = obj.cell_value;
+			break;
+		case param_type::string_param:
+			new (&string_value) std::basic_string<cell>(std::move(obj.string_value));
+			break;
+		case param_type::variant_param:
+			new (&variant_value) dyn_object(std::move(obj.variant_value));
+			break;
+		case param_type::handle_param:
+			new (&handle_value) std::shared_ptr<handle_t>(std::move(obj.handle_value));
+			break;
+		case param_type::expression_param:
+			new (&expr_value) expression_ptr(std::move(obj.expr_value));
+			break;
 	}
 }
 
@@ -83,23 +177,8 @@ stored_param &stored_param::operator=(const stored_param &obj) noexcept
 {
 	if(this != &obj)
 	{
-		if(type == param_type::string_param)
-		{
-			if(obj.type == type)
-			{
-				string_value = obj.string_value;
-			}else{
-				string_value.~basic_string();
-				cell_value = obj.cell_value;
-			}
-		}else if(obj.type == param_type::string_param)
-		{
-			new (&string_value) std::basic_string<cell>(obj.string_value);
-		}else{
-			cell_value = obj.cell_value;
-		}
-
-		type = obj.type;
+		stored_param::~stored_param();
+		new (this) stored_param(obj);
 	}
 	return *this;
 }
@@ -108,31 +187,27 @@ stored_param &stored_param::operator=(stored_param &&obj) noexcept
 {
 	if(this != &obj)
 	{
-		if(type == param_type::string_param)
-		{
-			if(obj.type == type)
-			{
-				string_value = std::move(obj.string_value);
-			}else{
-				string_value.~basic_string();
-				cell_value = obj.cell_value;
-			}
-		}else if(obj.type == param_type::string_param)
-		{
-			new (&string_value) std::basic_string<cell>(std::move(obj.string_value));
-		}else{
-			cell_value = obj.cell_value;
-		}
-
-		type = obj.type;
+		stored_param::~stored_param();
+		new (this) stored_param(std::move(obj));
 	}
 	return *this;
 }
 
 stored_param::~stored_param()
 {
-	if(type == param_type::string_param)
+	switch(type)
 	{
-		string_value.~basic_string();
+		case param_type::string_param:
+			string_value.~basic_string();
+			break;
+		case param_type::variant_param:
+			variant_value.~dyn_object();
+			break;
+		case param_type::handle_param:
+			handle_value.~shared_ptr<handle_t>();
+			break;
+		case param_type::expression_param:
+			expr_value.~expression_ptr();
+			break;
 	}
 }
--- plugins/src/objects/stored_param.h
+++ plugins/src/objects/stored_param.h
@@ -1,12 +1,15 @@
 #ifndef STORED_PARAM_H_INCLUDED
 #define STORED_PARAM_H_INCLUDED
 
+#include "modules/containers.h"
+#include "modules/expressions.h"
 #include "sdk/amx/amx.h"
 #include <string>
+#include <memory>
 
 enum class param_type
 {
-	cell_param, self_id_param, string_param
+	cell_param, self_id_param, string_param, variant_param, handle_param, expression_param
 };
 
 struct stored_param
@@ -15,6 +18,9 @@ struct stored_param
 	union {
 		cell cell_value;
 		std::basic_string<cell> string_value;
+		dyn_object variant_value;
+		std::shared_ptr<handle_t> handle_value;
+		expression_ptr expr_value;
 	};
 
 	static stored_param create(AMX *amx, char format, const cell *args, size_t &argi, size_t numargs);
@@ -23,6 +29,9 @@ struct stored_param
 	stored_param() noexcept;
 	stored_param(cell val) noexcept;
 	stored_param(std::basic_string<cell> &&str) noexcept;
+	stored_param(const dyn_object &obj) noexcept;
+	stored_param(std::shared_ptr<handle_t> &&handle) noexcept;
+	stored_param(expression_ptr &&expr) noexcept;
 	stored_param(const stored_param &obj) noexcept;
 	stored_param(stored_param &&obj) noexcept;
 	~stored_param();
