1c3e627cbe5565155f54a6321756cfb220f8a296
IllidanS4 <illidans4@gmail.com>
2022-10-09 13:07:12 +0200

    1.4.1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-PawnPlus v1.4
+PawnPlus v1.4.1
 ==========
 
 _PawnPlus_ extends the possibilities of the Pawn scripting language with new constructs, data types, and programming techniques.
--- plugins/src/main.h
+++ plugins/src/main.h
@@ -4,8 +4,8 @@
 #include "sdk/amx/amx.h"
 #include <utility>
 
-#define PP_VERSION_STRING "v1.4"
-#define PP_VERSION_NUMBER 0x140
+#define PP_VERSION_STRING "v1.4.1"
+#define PP_VERSION_NUMBER 0x141
 
 typedef void(*logprintf_t)(const char* format, ...);
 extern logprintf_t logprintf;
